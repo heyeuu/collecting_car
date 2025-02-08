83fd412ed9e7a7c6f65a5913a04495cff386f3f6
creeper5820 <cqq1960180796@163.com>
2025-02-08 10:58:38 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+constexpr uint8_t led_pin = 2;
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(led_pin, OUTPUT);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    digitalWrite(led_pin, HIGH);
+    delay(500);
+    digitalWrite(led_pin, LOW);
+    delay(500);
+}
